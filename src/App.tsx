3b675a2e79c99ea13f8c123c817f77d855d53e3b
Drew Polstra <dibujaron@gmail.com>
2020-08-03 21:47:07 -0400

    First trials with react-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Motion, spring } from 'react-motion';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Motion defaultStyle={{ width: 0 }} style={{ width: spring(100) }}>
+          {value => <img src={logo} style={value} className="App-logo" alt="logo" />}
+        </Motion>
+          <Motion defaultStyle={{ rotate: 0}} style={{ rotate: spring(180)}}>
+            {style => <p style={{transform: `rotate(${style.rotate}deg)`}}>Edit <code>src/App.tsx</code> and save to reload.</p>}
+          </Motion>
+          
         <a
           className="App-link"
           href="https://reactjs.org"
